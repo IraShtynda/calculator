c91fd444a3b46d6232401f9bc910f2254c71bfdc
Iryna Shtynda <ira.shtynda@gmail.com>
2023-02-23 15:05:58 +0200

    create project

---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+interface Props {
+    value: string;
+    onClick?: () => void;
+}
+
+const Button: FC<Props> = ({value, onClick}): JSX.Element => {
+  return <button className="calc-btn border" onClick={onClick}>{value}</button>
+};
+
+export default Button;
